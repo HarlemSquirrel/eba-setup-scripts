2499d35459d77fa13f38a0d00d3fc73553c12edb
Kevin McCormack <wittyman37@yahoo.com>
2016-09-09 12:07:31 -0400

    cleanups

--- update-target-pupil.sh
+++ update-target-pupil.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # This scipt is designed to make updating individual machines on the local network a breeze by using ssh keys and disabling the password for sudo
 
-
 set -o errexit		# errexit: exit the script if any statement returns a non-true return value
 set -o errtrace		# ERR trap is inherited by shell functions
 set -o pipefail  	# trace ERR through pipes
@@ -23,10 +22,6 @@ ssh-copy-id eba@$target
 #ssh -t -t eba@$1.local sudo sed -i "s/NoDisplay=false/NoDisplay=true/g" "/etc/xdg/autostart/*.desktop"
 
 # Copy and run the setup scripts
-#scp ~/Dropbox/Scripts/{eba-setup-netrun.sh,eba-setup.sh,loaner-setup.sh,set-hostname.sh,ProxyEBA.sh,pupil-setup.sh,configs/10-network-manager.pkla} eba@$target.local:~
-#ssh -t -t eba@$target.local bash eba-setup-netrun.sh no-wget
-#scp -r . eba@$target:~
-#rsync -acru -e ssh ~/code/eba-setup-scripts eba@$target:~
 ssh -t -t eba@$target '
 	if [ -e eba-setup-scripts ]; then
 		cd eba-setup-scripts && git pull;
